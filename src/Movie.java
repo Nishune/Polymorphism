43779e3888999dd03b0a24a21c80874004042285
Rikard <rikardengstrom89@gmail.com>
2025-06-25 12:18:10 +0200

    working

--- src/Movie.java
+++ src/Movie.java
@@ -27,3 +27,36 @@ class Adventure extends Movie {
                 "Something Bad Happens");
     }
 }
+
+class Comedy extends Movie {
+
+    public Comedy(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf("..%s%n".repeat(3),
+                "Something funny happens",
+                "Something even funnier happens.",
+                "happy ending");
+    }
+}
+
+class ScienceFiction extends Movie {
+
+    public ScienceFiction(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf("..%s%n".repeat(3),
+                "Bad Aliends do bad stuff",
+                "Space guys chase aliens",
+                "Planet blows up");
+    }
+}
+
