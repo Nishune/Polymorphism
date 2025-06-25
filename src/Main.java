25745b084211f03ed3a2b9a99bab785064713322
Rikard <rikardengstrom89@gmail.com>
2025-06-25 12:54:15 +0200

    fixed movie

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
 
-        Movie movie = Movie.getMovie("Adventure", "Star Wars");
+//        Movie movie = Movie.getMovie("Adventure", "Star Wars");
+//        movie.watchMovie();
 
-        movie.watchMovie();
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+            System.out.println("Enter Type (A for Adventure, C for Comedy, " + "S for Science Fiction, or Q to quit): ");
+            String type = sc.nextLine();
+            if ("Qq".contains(type)){
+                break;
+            }
+            System.out.println("Enter Movie Title: ");
+            String title = sc.nextLine();
+            Movie movie = Movie.getMovie(type, title);
+            movie.watchMovie();
+
+        }
 
     }
 }
